ef215869ede360b53d0423590791c5ff6ec1f355
valeriuhudea <valeriu.hudea@gmail.com>
2023-03-21 21:35:45 +0200

    last

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,30 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-interface filterRequest extends NextApiRequest {
-    body: any
-}
-
-export default async function handler(req: filterRequest, res: NextApiResponse) {
-    if (req.method !== "POST") {
-      res.status(405).send("Method not allowed")
-      return
-    }
-    const reqData = req.body
-  
-    if(reqData) {
-        try {
-            var urls = []
-            var urlsData = []
-            Object.keys(reqData).forEach(async function(key, index) {
-                console.log(reqData[key][0])
-                reqData[key][0].forEach(rslt => urls.push(rslt.link))
-            })
-            return res.status(200).json({ ai: urls });
-        } catch(e){
-            console.log(e)
-        }
-        return res.status(200).json({ ai: "test" });
-    } else {
-        return res.status(200).json({ ai: "failed" });
-    }
-}
+export default function handler(req, res) {
+    res.status(200).json({ name: 'John Doe' })
+  }
